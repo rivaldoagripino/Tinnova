3389c106a0688719a56120264232be7d97d50d7c
Rivaldo Pedro <81822512+rivaldoagripino@users.noreply.github.com>
2021-05-30 22:35:50 -0300

    Atividade 5 - Em Andamento

---
+++ ATV05/src/main/java/ATV05/rest/api/Principal.java
@@ -0,0 +1,13 @@
+package ATV05.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Principal {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Principal.class, args);
+	}
+
+}
---
+++ ATV05/src/main/java/ATV05/rest/api/repository/VeiculoRepository.java
@@ -0,0 +1,7 @@
+package ATV05.rest.api.repository;
+
+import ATV05.rest.api.model.VeiculoModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VeiculoRepository extends CrudRepository<VeiculoModel, Integer> {
+}
