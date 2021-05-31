e6ca778d122e81610195be7fff1f4c582f028c90
Rivaldo Pedro <81822512+rivaldoagripino@users.noreply.github.com>
2021-05-30 22:34:59 -0300

    Delete API directory

--- API/src/main/java/ATV05/rest/api/repository/VeiculoRepository.java
+++
@@ -1,7 +0,0 @@
-package ATV05.rest.api.repository;
-
-import ATV05.rest.api.model.VeiculoModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VeiculoRepository extends CrudRepository<VeiculoModel, Integer> {
-}
